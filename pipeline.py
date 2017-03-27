7b55d8cb1505e36a926e188d92ff0b2a09df776c
Panciera <michael.panciera@amedrhcwrair001.amed.ds.army.mil>
2017-03-27 16:03:52 -0400

    added logger.

--- pipeline.py
+++ pipeline.py
@@ -1,7 +1,10 @@
-
 '''
 Usage:
-      pipeline.py <r1> <r2> --config <config> [-o <outdir>]
+    pipeline.py <r1> <r2> --config <config> [-o <outdir>] [--log <log>]
+
+Options:
+    -c <config>, --config <config>
+    --log <log>
 '''
 import sh
 import itertools
@@ -9,6 +12,8 @@ from toolz.dicttoolz import keymap,valfilter
 from docopt import docopt
 from path import Path
 import types
+import yaml
+import sys
 # TODO: LZW!
 
 
@@ -16,11 +21,11 @@ import types
 # Utilities #
 #############
 class Config:
-    def __init__(self, **entries):
+    def __init__(self, entries):
         self.__dict__.update(entries)
         for k,v in self.__dict__.items():
             if type(v) == types.DictType:
-                setattr(self, k, Struct(**v))
+                setattr(self, k, Config(v))
 class Sh_(object):
     def __getattr__(self, attr):
         #cmd = getattr(sh, attr)
@@ -29,51 +34,52 @@ class Sh_(object):
             bools = valfilter(lambda x: type(x) is bool, kwargs)
             vargs = keymap("-{}".format, valfilter(lambda x: not type(x) is bool, kwargs))
             #bools.update(vargs)
-            print bools
             fixedargs = itertools.chain(vargs.items())
             getattr(sh, attr)(*(list(args) + list(fixedargs)), **bools)
         return command
 sh_ = Sh_()
 def unlist(*xs):
-  return " ".join(xs)
+  return " ".join(map(str, xs))
 
 ############
 # Parts    #
 ############
 
-def star(cfg, in1, in2):
-  sh.STAR(readFilesIn=unlist(in1, in2),
+def star(log, cfg, in1, in2):
+  sh.STAR('--readFilesIn', in1, in2, #readFilesIn=unlist(in1, in2),
           genomeDir=cfg.star.starDB,
-          outSamype="SAM",
-          outReadsUnmapped="Fastx")
+          outSAMtype="SAM",
+          outReadsUnmapped="Fastx",
+          _out=log, _err=log)
 
-def pricefilter(cfg, in1, in2, o1, o2):
-    cfg = cfg['pricefilter']
+def pricefilter(log, cfg, in1, in2, o1, o2):
+    cfg = cfg.pricefilter
     sh_.PriceSeqFilter(fp=unlist(in1, in2),
                        op=unlist(o1, o2),
                        rnf=cfg.calledPercent,
                        rqf=unlist(cfg.highQualPercent, cfg.highQualMin))
 
-def cdhitdup(cfg, r1, r2, o1, o2):
-    sh_.cd_hit_dup(i=r1, i2=r2, o=o1, o2=o2, e=cfg.cdhitdup.minDifference)
+def cdhitdup(log, cfg, r1, r2, o1, o2):
+    sh_.cd_hit_dup(i=r1, i2=r2, o=o1, o2=o2, e=cfg.cdhitdup.minDifference, _err=log, _out=log)
 
 # LZW!
 
-def bowtie_sensitive(cfg, r1, r2, o1, log):
+def bowtie_sensitive(log, cfg, r1, r2, o1):
     args = {'1' : r1, '2' : r2,
                 'very_sensitive_local' : True,
                 'un_conc' : Path(o1).splitext()[0],
-                'x' : cfg.bowtie2.bowtieDB}
+                'x' : cfg.bowtie2.bowtieDB,
+            '_err' : log, '_out' : log}
     sh.bowtie2(**args)
 
-def rapsearch(cfg, fq, out):
-    sh.rapsearch(o=out, d=cfg.rapsearch.rapsearchDB, q=fq)
+def rapsearch(log, cfg, fq, out):
+    sh.rapsearch(o=out, d=cfg.rapsearch.rapsearchDB, q=fq, _err=log, _out=log)
 
-def blastn(cfg, fq, out):
-    sh_.blastn(outfmt=8, db=cfg.ncbi.ntDB, o=out, q=fq)
+def blastn(log, cfg, fq, out):
+    sh_.blastn(outfmt=8, db=cfg.ncbi.ntDB, o=out, q=fq, _err=log, _out=log)
 
-def krona(cfg, blast, out):
-    sh.ktImportBlast(blast, o=out) # probably need config for kronadb!
+def krona(log, cfg, blast, out):
+    sh.ktImportBlast(blast, o=out, _err=log, _out=log) # probably need config for kronadb!
 
 
 ############
@@ -81,9 +87,8 @@ def krona(cfg, blast, out):
 ############
 
 
-star1 = "star.r1.fq"
-star2 = "star.r2.fq"
-
+star1 = "Unmapped.out.mate1"
+star2 = "Unmapped.out.mate2"
 psf1 = "psf.r1.fq"
 psf2 = "psf.r2.fq"
 
@@ -103,32 +108,40 @@ kronaNT1  = "r1.NT.html"
 kronaNT2  = "r2.NT.html"
 kronaNR1  = "r1.NR.html"
 kronaNR2  = "r2.NR.html"
-def run(cfg, input1, input2):
-  star(cfg, input1, input2)
+def run(cfg, input1, input2, log=None):
+  if not log:
+      log = sys.stdout
 
-  pricefilter(cfg, star1, star2, psf1, psf2)
+  star(log, cfg, input1, input2)
 
-  cdhitdup(cfg, psf1, psf2, cd1, cd2)
+  pricefilter(log, cfg, star1, star2, psf1, psf2)
 
-  bowtie_sensitive(cfg, cd1, cd2, bowtie1)
+  cdhitdup(log, cfg, psf1, psf2, cd1, cd2)
 
-  blastn(cfg, bowtie1, nt1)
-  blastn(cfg, bowtie2, nt2)
+  bowtie_sensitive(log, cfg, cd1, cd2, bowtie1)
 
-  rapsearch(cfg, bowtie1, nr1)
-  rapsearch(cfg, bowtie2, nr2)
+  blastn(log, cfg, bowtie1, nt1)
+  blastn(log, cfg, bowtie2, nt2)
 
-  krona(cfg, nt1, kronaNT1)
-  krona(cfg, nt2, kronaNT2)
-  krona(cfg, nt1, kronaNR1)
-  krona(cfg, nt2, kronaNR2)
+  rapsearch(log, cfg, bowtie1, nr1)
+  rapsearch(log, cfg, bowtie2, nr2)
+
+  krona(log, cfg, nt1, kronaNT1)
+  krona(log, cfg, nt2, kronaNT2)
+  krona(log, cfg, nr1, kronaNR1)
+  krona(log, cfg, nr2, kronaNR2)
 
 def main():
   args = docopt(__doc__, version='Version 1.0')
-  cfg = args['<config>']
-  cfg = yaml.load(open('template.yaml'))
+  cfg = args['--config']
+  cfg = yaml.load(open(cfg))
   cfg = Config(cfg)
-  run(cfg, args['<r1>'], args['<r2>'])
+  if args['--log']:
+   with open(args['--log'], 'a') as log:
+     run(cfg, args['<r1>'], args['<r2>'], log)
+  else:
+    run(cfg, args['<r1>'], args['<r2>'], log=None)
+
   sys.exit(0)
 
 if __name__ == '__main__': main()
